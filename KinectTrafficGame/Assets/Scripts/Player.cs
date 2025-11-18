99faba7d57d159a1c8ee805af08ffd21817ec5da
kflores33 <kflores3@inside.artcenter.edu>
2025-11-17 20:03:45 -0800

    Update Player.cs

--- KinectTrafficGame/Assets/Scripts/Player.cs
+++ KinectTrafficGame/Assets/Scripts/Player.cs
@@ -117,25 +117,25 @@ public class PlayerTrafficController : MonoBehaviour
         if (kb == null) return;
 
         // A = 左转
-        if (KinectInputSystem.GetButtonDown("Kinect_LeftHandHorizontal"))
+        if (KinectInputSystem.GetButtonDown("Kinect_LeftHandHorizontal") || kb.aKey.wasPressedThisFrame)
         {
             SendCommandToCurrentLane(CarCommandType.Right);
             Debug.Log("left hand raised to side, signal car to turn right");
         }
         // W = 直行
-        else if (KinectInputSystem.GetButtonDown("Kinect_LeftHandRaised"))
+        else if (KinectInputSystem.GetButtonDown("Kinect_LeftHandRaised") || kb.wKey.wasPressedThisFrame)
         {
             SendCommandToCurrentLane(CarCommandType.Straight);
             Debug.Log("left hand is up; let cars go straight");
         }
         // D = 右转
-        else if (KinectInputSystem.GetButtonDown("Kinect_RightHandHorizontal"))
+        else if (KinectInputSystem.GetButtonDown("Kinect_RightHandHorizontal") || kb.dKey.wasPressedThisFrame)
         {
             SendCommandToCurrentLane(CarCommandType.Left);
             Debug.Log("Right hand raised to side, signal car to turn left");
         }
         // S = 停止
-        else if (KinectInputSystem.GetButtonDown("Kinect_RightHandRaised"))
+        else if (KinectInputSystem.GetButtonDown("Kinect_RightHandRaised") || kb.sKey.wasPressedThisFrame)
         {
             SendCommandToCurrentLane(CarCommandType.Stop);
             Debug.Log("Right hand is up; stop cars");
